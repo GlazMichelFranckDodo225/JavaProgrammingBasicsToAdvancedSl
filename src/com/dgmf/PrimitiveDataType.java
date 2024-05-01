67d9aed2469bed47be807c876f291bd0ac6c34ee
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-05-01 09:28:09 +0200

    Primitive Data Types

---
+++ src/com/dgmf/PrimitiveDataType.java
@@ -0,0 +1,24 @@
+package com.dgmf;
+
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		char a = 'A';
+		byte b = 2;
+		short c = 22;
+		int d = 45;
+		float e = 5.123451F;
+		double f = 4.68295233535D;
+		boolean g = true;
+		
+		System.out.println("char : " + a);
+		System.out.println("byte : " + b);
+		System.out.println("short : " + c);
+		System.out.println("int : " + d);
+		System.out.println("float : " + e);
+		System.out.println("double : " + f);
+		System.out.println("boolean : " + g);
+
+	}
+
+}
